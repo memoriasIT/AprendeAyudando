ccc6bf178e7210d99898b902c1d2f0e183ae4cb2
merlindaniel <merlindaniel30@gmail.com>
2020-11-28 14:17:19 +0100

    La pagina requestpermissions es accesible desde activity y courses

--- AprendeAyudando/request_permissions/urls.py
+++ AprendeAyudando/request_permissions/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'courses'
+app_name = 'request_permissions'
 urlpatterns = [
     path('', views.index, name='index'), #/requestpermissions
 ]
