5da29d419dff685430bbce2703a6ec6fe180a89c
merlindaniel <merlindaniel30@gmail.com>
2020-11-30 21:48:36 +0100

    Ahora una entidad puede ser tambien el creador del foro y se elimina enrolled_users de Forum

--- AprendeAyudando/forum/models.py
+++ AprendeAyudando/forum/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.db.models import Q
 
 class Forum(models.Model):
     title = models.CharField(max_length=200)
-    enrolled_users = models.ManyToManyField(get_user_model(), related_name="forums", blank=True)
-    teacher = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE, limit_choices_to={'groups__name':'Profesor'})
+    #enrolled_users = models.ManyToManyField(get_user_model(), related_name="forums", blank=True)
+    teacher = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE, 
+                                limit_choices_to=Q(groups__name='Profesor') | Q(groups__name='EntidadPublicoPrivada'))
 
     def __str__(self):
         return self.title
