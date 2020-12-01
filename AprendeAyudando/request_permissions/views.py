6fa5858ff3fe18c6d1b2a7bceec09d00f03e3fa1
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2020-12-01 23:32:57 +0100

    no permission.html

--- AprendeAyudando/request_permissions/views.py
+++ AprendeAyudando/request_permissions/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import get_object_or_404
 @login_required
 @permission_required('request_permissions.add_request_permissions', raise_exception=True)
 def index(request):
-    return HttpResponse("Solo puede entrar un Alumno o Admin")
+    return render(request, 'request_permissions/no_permission.html')
 
 @login_required
 @permission_required('request_permissions.delete_request_permissions', raise_exception=True)
