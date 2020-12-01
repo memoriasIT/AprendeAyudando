20c928aad6c5e8a00a10e7a2963de3ab130c2c2f
Majo Chaves <majochavesma@gmail.com>
2020-12-01 17:17:08 +0100

    pagina curso y actividad

--- AprendeAyudando/forum/admin.py
+++ AprendeAyudando/forum/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Forum
 
 
-class AdminForum(admin.ModelAdmin):
-    list_display=("title","teacher")
+class AdminForum(admin.ModelAdmin): 
+    pass
+    list_display=("title","author")
     search_fields=("title",)   #Para realizar barra de busqueda
 
 admin.site.register(Forum,AdminForum)
