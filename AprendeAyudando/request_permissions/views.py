7f149f789c68c9cefd405a7c57cf44009007954c
zodd18 <josiesala@gmail.com>
2020-12-01 21:09:30 +0100

    DONE: view and deny permissions requests, WIP: accept requests

--- AprendeAyudando/request_permissions/views.py
+++ AprendeAyudando/request_permissions/views.py
@@ -10,3 +10,16 @@ from .models import Request_permissions
 @permission_required('request_permissions.add_request_permissions', raise_exception=True)
 def index(request):
     return HttpResponse("Solo puede entrar un Alumno o Admin")
+
+def give_permissions(request):
+    ctx = { "permission_requests": Request_permissions.objects.all() } 
+    return render(request, 'request_permissions/give_permissions.html', ctx)
+
+def accept(request, request_id):
+    # TODO accept then delete:
+    Request_permissions.objects.get(pk=request_id).delete()
+    pass
+
+def deny(request, request_id):
+    Request_permissions.objects.get(pk=request_id).delete()
+    return give_permissions(request)
