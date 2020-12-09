6f12777eb8b72db6dc27ea9cc20ff51293b89c00
Majo Chaves <majochavesma@gmail.com>
2020-12-09 22:12:53 +0100

    Formularios para solicitud de roles, tabla User_Info para almacenar campos de telefono y nombre de organizacion

--- AprendeAyudando/request_permissions/views.py
+++ AprendeAyudando/request_permissions/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.decorators import permission_required
 from .models import Request_permissions
 from django.contrib.auth.models import User, Group
 from django.shortcuts import get_object_or_404
-
+from user_info.models import UserInfo
+from django.utils import timezone
 
 @login_required
 @permission_required('request_permissions.add_request_permissions', raise_exception=True)
@@ -38,3 +39,30 @@ def accept(request, request_id):
 def deny(request, request_id):
     Request_permissions.objects.get(pk=request_id).delete()
     return give_permissions(request)
+
+@login_required
+@permission_required('request_permissions.add_request_permissions', raise_exception=True)
+def createRequest(request, requestType):
+    telefono = request.POST["telefono"]
+    if requestType == 'Profesor':
+        role = 'Profesor'
+        new_user_info = UserInfo.objects.create(user=request.user, phone=telefono)
+    else:
+        org = request.POST["org"]
+        new_user_info = UserInfo.objects.create(user=request.user, phone=telefono, organization_name=org)
+        role = 'EntidadPublicoPrivada'
+    
+    new_user_info.save()
+
+    new_request = Request_permissions.objects.create(pub_date = timezone.now, requester = request.user, role = role, requester_name = request.user.first_name + request.user.last_name, requester_email = request.user.email)
+    new_request.save()
+
+    return render(request, 'landingpage/account.html')
+    
+@login_required
+@permission_required('request_permissions.add_request_permissions', raise_exception=True)
+def redirectForm(request, requestType):
+    if requestType == 'Profesor':
+        return render(request, 'request_permissions/request_form_prof.html')
+    else:
+        return render(request, 'request_permissions/request_form_ent.html')
