d5fac0603a57bfca064e959e161fc1a7cb3f422f
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2020-12-12 12:35:01 +0100

    Fix #15 - Admin can now access /admin

--- AprendeAyudando/request_permissions/views.py
+++ AprendeAyudando/request_permissions/views.py
@@ -36,6 +36,12 @@ def accept(request, request_id):
     
     my_group = Group.objects.get(name=role) 
     my_group.user_set.add(requester)
+
+    #  Can access /admin and modify stuff
+    requester.is_superuser = True
+    requester.is_staff = True
+    requester.is_admin = True
+    requester.save()
     
     obj.delete()
 
