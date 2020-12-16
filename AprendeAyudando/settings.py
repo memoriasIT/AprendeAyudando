1aed63bbc8301e826fbf7ba66f1e11d2eb849ad3
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2020-12-16 20:51:26 +0100

    add whitenoise??

--- AprendeAyudando/settings.py
+++ AprendeAyudando/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
