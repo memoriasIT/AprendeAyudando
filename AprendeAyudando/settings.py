10ca70cbae008cce8950a7299172c2dc13850036
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2020-12-16 20:46:21 +0100

    add mediaroot

--- AprendeAyudando/settings.py
+++ AprendeAyudando/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/' #os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
