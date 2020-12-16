c70ca5b7b7c31f730c38c3f502bf50090fd649f3
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2020-12-16 19:55:30 +0100

    fix staticroot

--- AprendeAyudando/settings.py
+++ AprendeAyudando/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/staticOutput/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     str(BASE_DIR.joinpath('AprendeAyudando/static')),
 ]
