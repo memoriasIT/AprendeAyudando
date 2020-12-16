172dd1a0a9bb8e9ffd32a89e320d8801570d42f6
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2020-12-16 20:09:19 +0100

    use django-heroku

--- AprendeAyudando/settings.py
+++ AprendeAyudando/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import os
+import django_heroku
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
