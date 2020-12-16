cac8dbba02704a745840502b2ed561c956ddc868
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2020-12-16 20:39:30 +0100

    change basedir

--- AprendeAyudando/settings.py
+++ AprendeAyudando/settings.py
@@ -5,7 +5,7 @@ import os
 # django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve()#.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
