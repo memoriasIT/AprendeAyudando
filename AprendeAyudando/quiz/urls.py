a7c0106ae0c777b2a6b49d1f3c21bb1dcad227b6
merlindaniel <merlindaniel30@gmail.com>
2020-12-25 22:10:53 +0100

    Ahora se puede crear test en un curso

--- AprendeAyudando/quiz/urls.py
+++ AprendeAyudando/quiz/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'quiz'
 urlpatterns = [
-    path('create/course/<int:course_id>/', views.createQuizCourse, name='createQuizCourse'),
-    path('create/course/<int:course_id>/<int:quiz_course_id>/<int:number_questions>/', views.createQuestionsCourse, name='createQuestionsCourse'),
-    #path('delete/<int:forum_id>/', views.delete, name='delete'), # /forum/delete/1/
+    path('create/quizcourse/<int:course_id>/', views.createQuizCourse, name='createQuizCourse'),
+    path('create/quizcourse/<int:course_id>/<int:quiz_course_id>/<int:number_questions>/', views.createQuestionsCourse, name='createQuestionsCourse'),
+    path('create/quizcourse/<int:course_id>/<int:question_course_id>/<int:number_questions>/<int:number_answers>/', views.createAnswersCourse, name='createAnswersCourse'), # /forum/delete/1/
 
 ]
--- AprendeAyudando/quiz/views.py
+++ AprendeAyudando/quiz/views.py
@@ -13,7 +13,7 @@ from django.http import HttpResponseForbidden
 
 #Models
 from courses.models import Course
-from quiz.models import Quiz, QuizCourse, QuestionCourse
+from quiz.models import Quiz, QuizCourse, QuestionCourse, AnswerCourse
 
 @login_required
 @permission_required('quiz.add_quizcourse', raise_exception=True)
@@ -48,9 +48,9 @@ def createQuizCourse(request, course_id):
         new_quiz.save()
 
         ctx['quiz'] = new_quiz
-        ctx['number_questions'] = int(number_questions) - 1
+        ctx['number_questions'] = int(number_questions)
         return render(request, 'quiz/createquestion.html', ctx)
-    return render(request, 'quiz/createquiz.html', ctx)#HttpResponse("Esta es la futura pagina de crear QUIZ para el curso: %s." %course.title)
+    return render(request, 'quiz/createquiz.html', ctx)
 
 @login_required
 @permission_required('quiz.add_questioncourse', raise_exception=True)
@@ -75,24 +75,70 @@ def createQuestionsCourse(request, course_id, quiz_course_id, number_questions):
         'number_questions': number_questions,
     }
 
-    if(number_questions==0):
-        return HttpResponse("Se termino las preguntas")
-    
     #-------------------------------------POST FORM-------------------------------------------
     if request.method=="POST":
         new_question_text = request.POST["new_question_text"]
         new_question_score = request.POST["new_question_score"]
-        
+        number_answers = request.POST["number_answers"]
         new_question = QuestionCourse.objects.create(
             text=new_question_text,
             question_score=new_question_score,
             quiz=quiz
         )
         new_question.save()
-        ctx['number_questions'] = number_questions - 1
-        render(request, 'quiz/createquestion.html', ctx)
+
+        ctx['question'] = new_question
+        ctx['number_answers'] = int(number_answers)
+        ctx['range_number_answers'] = range(int(number_answers))
+        return render(request, 'quiz/createanswers.html', ctx)
     
     #-----------------------------------------Default(first time)-------------------------------------
     
     return render(request, 'quiz/createquestion.html', ctx)
+
+@login_required
+@permission_required('quiz.add_questioncourse', raise_exception=True)
+def createAnswersCourse(request, course_id, question_course_id, number_questions, number_answers):
+    course = get_object_or_404(Course, pk=course_id)
+    question = get_object_or_404(QuestionCourse, pk=question_course_id)
+
+    #-----------------------------------CONTROL DE ACCESO-----------------------------------
+    isOwner = False
+    if is_owner(request.user, course.teacher):
+        isOwner = True
+    
+    if not isOwner and not request.user.is_superuser:
+        return HttpResponseForbidden()
+    
+    #-----------------------------------ELEMENTOS PARA HTML-----------------------------------
+    ctx = {
+        'is_course':True,
+        'isOwner':isOwner,
+        'course': course,
+        'quiz': question.quiz,
+        'number_answers': number_answers,
+    }
+
+    #----------------------------------------FORM---------------------------------------------
+    if request.method=="POST":
+        checked_values = request.POST.getlist('new_answer_is_correct[]')
+        
+        for x in range(number_answers):
+            new_answer_text = request.POST["new_answer_text"+str(x)]
+            new_answer_is_correct = False
+            if str(x) in checked_values:
+                new_answer_is_correct = True
+            new_answer = AnswerCourse.objects.create(
+                text=new_answer_text,
+                correct=new_answer_is_correct,
+                question=question
+            )
+            new_answer.save()
+        
+        number_questions = number_questions - 1
+        ctx['number_questions'] = number_questions
+        return render(request, 'quiz/createquestion.html', ctx)
+
+    return render(request, 'quiz/createanswers.html', ctx)
+
     
