c33bfd1ef8b7d947fc8632c70747f152ac33310f
Majo Chaves <majochavesma@gmail.com>
2020-12-26 21:51:51 +0100

    Debates en los foros (crear, ver y borrar)

--- AprendeAyudando/forum/admin.py
+++ AprendeAyudando/forum/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Forum
+from .models import Forum, Debate, Message
 
 
 class AdminForum(admin.ModelAdmin): 
@@ -7,4 +7,16 @@ class AdminForum(admin.ModelAdmin):
     list_display=("title","author")
     search_fields=("title",)   #Para realizar barra de busqueda
 
+class AdminDebate(admin.ModelAdmin):
+    list_display=("title","author")
+    search_fields=("title",)   #Para realizar barra de busqueda
+
+class AdminMessage(admin.ModelAdmin):
+    list_display=("author", "debate")
+    search_fields=("author",)   #Para realizar barra de busqueda
+    date_hierarchy="pub_date"
+
+
 admin.site.register(Forum,AdminForum)
+admin.site.register(Debate,AdminDebate)
+admin.site.register(Message,AdminMessage)
