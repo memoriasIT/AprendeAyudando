4de8a68a4619a917c23167f96558c2e37fc9f9c0
merlindaniel <merlindaniel30@gmail.com>
2020-12-27 19:08:40 +0100

    Se elimina antiguos Permissions de clases que ya no existen

--- AprendeAyudando/quiz/admin.py
+++ AprendeAyudando/quiz/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import Quiz, Question, Answer, Qualification, QuestionAsked
-
 class AdminQuiz(admin.ModelAdmin):
     list_display=("title","course","activity","id")
     search_fields=("title",)
-    
+
 class AdminQuestion(admin.ModelAdmin):
     list_display=("text","question_score","quiz","id")
     search_fields=("text","quiz",)
@@ -21,7 +20,6 @@ class AdminQuestionAsked(admin.ModelAdmin):
     list_display=("qualification","question","id")
     search_fields=("qualification","question",)
 
-admin.site.register(Quiz,AdminQuiz)
 admin.site.register(Question,AdminQuestion)
 admin.site.register(Answer,AdminAnswer)
 admin.site.register(Qualification,AdminQualification)
