52144ced1e55dce4a5115f0a7078d9ed23f8b8bc
fraaaandelpino <fcodelpino98@gmail.com>
2020-12-29 18:45:29 +0100

    Editar datos del usuario

--- AprendeAyudando/AprendeAyudando/forms.py
+++ AprendeAyudando/AprendeAyudando/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.db import models
+from django.forms import ModelForm
 
 class RegisterForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
@@ -10,3 +13,15 @@ class RegisterForm(forms.Form):
 
 class RecoverForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+
+class UploadForm(forms.ModelForm):
+    username = forms.CharField(disabled=True)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}), required=False)
+    password_repeat = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}), required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_repeat', 'first_name', 'last_name']
