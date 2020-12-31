ae84791c0ad3293162572ef6f5eca9313fbc968e
merlindaniel <merlindaniel30@gmail.com>
2020-12-31 15:56:55 +0100

    Los campos largos pasan de CharField a TextField

--- AprendeAyudando/messaging/models.py
+++ AprendeAyudando/messaging/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 class MessagingMessage(models.Model):
     title = models.CharField(max_length=100)
-    text = models.CharField(max_length=1000, blank=True, null=True)
+    text = models.TextField(max_length=2000, blank=True, null=True)
     message_sent_date = models.DateTimeField(verbose_name="message sent date", auto_now_add=True)
     user_origin = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='messaging_message_user_origin')
     user_destination = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='messaging_message_user_destination')
