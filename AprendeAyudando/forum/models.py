d4ba3cdec627de53591da41969b9e698995fff71
Majo Chaves <majochavesma@gmail.com>
2021-01-03 21:02:18 +0100

    Foros con descripcion

--- AprendeAyudando/forum/models.py
+++ AprendeAyudando/forum/models.py
@@ -9,6 +9,7 @@ class Forum(models.Model):
                                 limit_choices_to=Q(groups__name='Profesor') | Q(groups__name='EntidadPublicoPrivada'))
     activityCourseFk = models.IntegerField()
     activityCourseType = models.CharField(max_length=20)
+    description = models.CharField(max_length=1000, blank=True)
     def __str__(self):
         return self.title
     
